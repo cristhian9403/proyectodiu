1f4ab4d98e1434ea20d38c1917bc6ef2b6a4d6dc
cristhian9403 <cristhian.camilo.alvarez@correounivalle.edu.co>
2015-06-09 16:00:13 -0500

    accesibilidad

--- js/accesibilidad.js
+++ js/accesibilidad.js
@@ -1,14 +1,60 @@
 /* Javascript */
-function accesibiliza(){
-    var body = document.getElementsByTagName("div")[0];
-    var class_name = "accesible";
-    var bot = document.getElementById("bot-accesible");
-    var ya_tiene = new RegExp('\b'+class_name+'\b').test(body.className);
-    if(ya_tiene){//ya la tiene. Remueve.
-	var rep = body.className.match(' '+class_name)?' '+class_name:class_name;
-        body.className = body.className.replace(rep,'');
-    }
-    else {//no tiene, agrega clase.
-	body.className += " "+class_name;
-    }
+var Counter = 0;
+function ResizeText(size) 
+{
+if ((size == '10') && (Counter == 0)) return;
+var HtmlTags = document.getElementById('PostContent').getElementsByTagName('*');
+
+for (i = 0; i < HtmlTags.length; i++) 
+{
+if (HtmlTags[i].tagName == "H4") 
+{
+if (size == '8') 
+{
+HtmlTags[i].style.fontSize = 10 + "pt"
+}
+else if (size == '10') 
+{
+HtmlTags[i].style.fontSize = 12 + "pt"
+}
+else if (size == '12') 
+{
+HtmlTags[i].style.fontSize = 14 + "pt"
+}
+}
+else if (HtmlTags[i].tagName == "td") 
+{
+if (size == '8')
+HtmlTags[i].style.fontSize = 14 + "pt"
+else if (size == '10')
+HtmlTags[i].style.fontSize = 16 + "pt"
+else if (size == '12')
+HtmlTags[i].style.fontSize = 18 + "pt"
+}
+else 
+{
+HtmlTags[i].style.fontSize = size + "pt"
+}
+}
+
+if (size == '8') 
+{
+Counter = 1;
+document.getElementById('SmallFont').style.backgroundColor = 'orange';
+document.getElementById('MediumFont').style.backgroundColor = 'yellow';
+document.getElementById('LargeFont').style.backgroundColor = 'yellow';
+}
+else if (size == '10') 
+{
+document.getElementById('SmallFont').style.backgroundColor = 'yellow';
+document.getElementById('MediumFont').style.backgroundColor = 'orange';
+document.getElementById('LargeFont').style.backgroundColor = 'yellow';
+}
+else if (size == '12') 
+{
+Counter = 1;
+document.getElementById('SmallFont').style.backgroundColor = 'yellow';
+document.getElementById('MediumFont').style.backgroundColor = 'yellow';
+document.getElementById('LargeFont').style.backgroundColor = 'orange';
+}
 }
